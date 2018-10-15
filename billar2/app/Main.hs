987ed17cab25dda0efa05acc22b8fff7188af607
Jose Seraquive <jose.seraquive@gmail.com>
2018-10-15 15:44:03 -0500

    Better organization of the code

--- billar2/app/Main.hs
+++ billar2/app/Main.hs
@@ -4,40 +4,57 @@ import Graphics.Gloss as G
 import Graphics.Gloss.Interface.Pure.Game
 -- |Parameters
 
+width, height, offset :: Int
+width = 300
+height = 300
+offset = 100
+
 -- |Functions ----------------------------------------------------------------------------------------------------------
+
+-- | Initial window
+window :: Display
+window =  InWindow "Billiard Game" (height, width)(offset,offset)
+
+background :: Color
+background = white
+
+
+
+initialWorld :: Ball
+initialWorld = Ball 1 10 20 0.1 2
+
+
 -- | Movement of the balls
-movementBall :: Ball -> Picture
-movementBall (Ball _ r _ _ _) = G.circleSolid r
+toPicture :: Ball -> Picture
+toPicture (Ball _ r _ _ _ ) = translate (-20)(100) $  color ballColor $ G.circleSolid r
+                               where ballColor = red
 
 
-iteracion :: Float -> Ball -> Ball
-iteracion _ b = b
+
+
+iteration :: Float -> Ball -> Ball
+iteration _ b = b
+
 
 
 evento :: Event -> Ball -> Ball
 evento _ x = x
 
 -- | Main --------------------------------------------------------------------------------------------------------------
-main = do play (InWindow "Billar Project" (700, 700)(20,20))
-              white
-              2
-              (Ball 1 20.0 3 0.5 2.0)
-              movementBall
-              evento
-              iteracion
+main = do play window
+              background              --  Background color
+              2                       -- Number of simulation steps to take for each second real time
+              initialWorld            -- Initial world
+              toPicture                 -- Convert the world a picture
+              evento                  -- Function to handle input events
+              iteration              -- A function to step the world one iteration. It is passed the period of time (in seconds) needing to be advanced
 
 
 
 -- | Types -------------------------------------------------------------------------------------------------------------
 
 -- | The game world
-data World
-        = World
-        { width         :: Double
-        , height        :: Double
-        , pixWidth      :: Int
-        , pixHeight     :: Int }
-         deriving Show
+
 
 
 -- | The billar's balls
@@ -46,13 +63,15 @@ data Ball = Ball {identifier  :: Int,
                   mass        :: Int,
                   miu         :: Float,
                   acceleration:: Float}
-                --  position    :: Vec2,
+                  --position    :: Vec2}
                   --velocity    :: Vec2}
-                   deriving Show
+                  deriving Show
+
+data Table = Table {tableHeight :: Int,
+                    tableWidth ::  Int }
+                    deriving Show
 
 -- | Vector for the billar's balls
 data Vec2 = Vec2 {x :: Double,
                     y :: Double}
                     deriving Show
-
-
